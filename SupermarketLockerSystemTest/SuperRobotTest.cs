c37006780d8bafa340a8123c64cfa1c07a7aec9a
hjyao <hjyao@thoughtworks.com>
2013-09-08 21:16:14 +0800

    hj:add super robot test

---
+++ SupermarketLockerSystemTest/SuperRobotTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using SupermarketLockerSystem;
+using Xunit;
+
+namespace SupermarketLockerSystemTest
+{
+    public class SuperRobotTest
+    {
+        [Fact]
+        public void should_return_ticket_when_store_bag()
+        {
+            var bag = new Bag();
+            var superRobot = new SuperRobot(1, 1);
+            Ticket ticket = superRobot.Store(bag);
+            Assert.NotNull(ticket);
+        }
+
+        [Fact]
+        public void should_pick_bag_with_ticket()
+        {
+            var expectedBag = new Bag();
+            var superRobot = new SuperRobot(1,1);
+            var ticket = superRobot.Store(expectedBag);
+            var bag = superRobot.Pick(ticket);
+            Assert.Equal(expectedBag, bag);
+        }
+
+    }
+
+    public class SuperRobot : Robot
+    {
+        public SuperRobot(int capacity, int boxesNumber) : base(capacity, boxesNumber)
+        {
+        }
+
+        public SuperRobot(List<Locker> lockers) : base(lockers)
+        {
+        }
+    }
+}
