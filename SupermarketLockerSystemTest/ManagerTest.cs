66bb02d97ee9a144b86003b2498d7f3cbcfbc2f5
hjyao <hjyao@thoughtworks.com>
2013-09-11 22:57:15 +0800

    hj support pick using manager

--- SupermarketLockerSystemTest/ManagerTest.cs
+++ SupermarketLockerSystemTest/ManagerTest.cs
@@ -3,6 +3,40 @@ using SupermarketLockerSystem;
 using Xunit;
 using System.Linq;
 
+namespace SupermarketLockerSystem
+{
+    public class Manager
+    {
+        private readonly List<Locker> lockers;
+        private readonly List<SmartRobot> robots;
+
+        public Manager(List<Locker> lockers, List<SmartRobot> robots)
+        {
+            this.lockers = lockers;
+            this.robots = robots;
+        }
+
+        public Ticket Store(Bag bag)
+        {
+            if(lockers.Any(l => l.IsAvailable))
+                return lockers.First(l => l.IsAvailable).Store(bag);
+            if (robots.Any(r => r.IsAvailable))
+                return robots.First(r => r.IsAvailable).Store(bag);
+            return null;
+        }
+
+        public Bag Pick(Ticket ticket)
+        {
+            foreach (var bag in lockers.Select(locker => locker.Pick(ticket)).Where(bag => bag != null))
+            {
+                return bag;
+            }
+            return robots.Select(robot => robot.Pick(ticket))
+                         .FirstOrDefault(b => b != null);
+        }
+    }
+}
+
 namespace SupermarketLockerSystemTest
 {
     public class ManagerTest
@@ -32,26 +66,25 @@ namespace SupermarketLockerSystemTest
             Assert.Equal(bag2, smartRobot.Pick(ticket2));
             Assert.Equal(bag3, superRobot.Pick(ticket3));
         }
-    }
 
-    public class Manager
-    {
-        private readonly List<Locker> lockers;
-        private readonly List<SmartRobot> robots;
-
-        public Manager(List<Locker> lockers, List<SmartRobot> robots)
+        [Fact]
+        public void should_pick_bag_given_existing_ticket()
         {
-            this.lockers = lockers;
-            this.robots = robots;
-        }
+            var locker = new Locker(1);
+            var lockers = new List<Locker> { locker };
+            var smartRobot = new SmartRobot(new List<Locker> {new Locker(1)});
+            var superRobot = new SuperRobot(new List<Locker> {new Locker(1)});
 
-        public Ticket Store(Bag bag)
-        {
-            if(lockers.Any(l => l.IsAvailable))
-                return lockers.First(l => l.IsAvailable).Store(bag);
-            if (robots.Any(r => r.IsAvailable))
-                return robots.First(r => r.IsAvailable).Store(bag);
-            return null;
+            var robots = new List<SmartRobot> {
+                smartRobot,
+                superRobot
+            };
+            var manager = new Manager(lockers, robots);
+            var expectedBag = new Bag();
+            var ticket = manager.Store(expectedBag);
+            var actualBag = manager.Pick(ticket);
+
+            Assert.Equal(expectedBag, actualBag);
         }
     }
 }
